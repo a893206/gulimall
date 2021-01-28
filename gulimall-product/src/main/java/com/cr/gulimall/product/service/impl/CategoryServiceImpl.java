8b90818170c5ebad0b1f6951fb31d47a2506997f
CR <931009686@qq.com>
2021-01-28 21:52:37 +0800

    商品服务-API-三级分类-查询-递归树形结构数据获取

--- gulimall-product/src/main/java/com/cr/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/cr/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,18 +1,24 @@
 package com.cr.gulimall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cr.common.utils.PageUtils;
 import com.cr.common.utils.Query;
-
 import com.cr.gulimall.product.dao.CategoryDao;
 import com.cr.gulimall.product.entity.CategoryEntity;
 import com.cr.gulimall.product.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
+/**
+ * @author cr
+ */
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
@@ -24,6 +30,38 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1、查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        //2、组装成父子的树形结构
+
+        //2.1、找到所有的一级分类
+        return entities.stream()
+                .filter((categoryEntity) -> categoryEntity.getParentCid() == 0)
+                .peek((menu) -> menu.setChildren(getChildren(menu, entities)))
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * 递归查找所有菜单的子菜单
+     *
+     * @param root 根结点
+     * @param all  所有菜单
+     * @return 子菜单
+     */
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        return all.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid().equals(root.getCatId()))
+                //1、找到子菜单
+                .peek(categoryEntity -> categoryEntity.setChildren(getChildren(categoryEntity, all)))
+                //2、菜单的排序
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
     }
 
 }
