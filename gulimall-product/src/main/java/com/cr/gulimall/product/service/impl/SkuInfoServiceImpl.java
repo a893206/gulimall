5722c6d8e36c10e9b1ba598bb7bd9b3cc506781b
CR <931009686@qq.com>
2021-06-27 03:05:53 +0800

    商品服务-API-商品管理-SKU检索

--- gulimall-product/src/main/java/com/cr/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/cr/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -8,6 +8,7 @@ import com.cr.common.utils.Query;
 import com.cr.gulimall.product.dao.SkuInfoDao;
 import com.cr.gulimall.product.entity.SkuInfoEntity;
 import com.cr.gulimall.product.service.SkuInfoService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -29,6 +30,39 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     @Override
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         baseMapper.insert(skuInfoEntity);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> wrapper = new QueryWrapper<>();
+
+        String key = (String) params.get("key");
+        if (StringUtils.isNotBlank(key)) {
+            wrapper.and((w) -> w.eq("sku_id", key).or().like("sku_name", key));
+        }
+        String brandId = (String) params.get("brandId");
+        if (StringUtils.isNotBlank(brandId) && !"0".equals(brandId)) {
+            wrapper.eq("brand_id", brandId);
+        }
+        String catalogId = (String) params.get("catalogId");
+        if (StringUtils.isNotBlank(catalogId) && !"0".equals(catalogId)) {
+            wrapper.eq("catalog_id", catalogId);
+        }
+        String min = (String) params.get("min");
+        if (StringUtils.isNotBlank(min) && !"0".equals(min)) {
+            wrapper.ge("price", min);
+        }
+        String max = (String) params.get("max");
+        if (StringUtils.isNotBlank(max) && !"0".equals(max)) {
+            wrapper.le("price", max);
+        }
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),
+                wrapper
+        );
+
+        return new PageUtils(page);
     }
 
 }
