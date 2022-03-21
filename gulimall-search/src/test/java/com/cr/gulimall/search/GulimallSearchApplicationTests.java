601eb172a9ab97d156c93ca3b0af3682c7e44aac
CR <931009686@qq.com>
2022-03-22 00:46:22 +0800

    feat: 126、全文检索-ElasticSearch-整合-测试保存

--- gulimall-search/src/test/java/com/cr/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/cr/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,6 +1,13 @@
 package com.cr.gulimall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.cr.gulimall.search.config.GulimallElasticSearchConfig;
+import lombok.Data;
+import lombok.SneakyThrows;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +24,39 @@ public class GulimallSearchApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println(client);
+    }
+
+    /**
+     * 测试存储数据到es
+     * 更新也可以
+     */
+    @SneakyThrows
+    @Test
+    public void indexData() {
+        IndexRequest indexRequest = new IndexRequest("users");
+        // 数据的id
+        indexRequest.id("1");
+//        indexRequest.source("userName", "zhangsan", "age", 18, "gender", "男");
+        User user = new User();
+        user.setUserName("zhangsan");
+        user.setAge(18);
+        user.setGender("男");
+        String jsonString = JSON.toJSONString(user);
+        // 要保存的内容
+        indexRequest.source(jsonString, XContentType.JSON);
+
+        // 执行操作
+        IndexResponse index = client.index(indexRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+
+        // 提取有用的响应数据
+        System.out.println(index);
+    }
+
+    @Data
+    class User {
+        private String userName;
+        private String gender;
+        private Integer age;
     }
 
 }
