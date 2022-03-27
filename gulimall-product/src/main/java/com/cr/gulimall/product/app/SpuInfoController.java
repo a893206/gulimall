3f4b73f129ad08d7357d381f4e5f16d4fb862341
CR <931009686@qq.com>
2022-03-27 17:31:48 +0800

    feat: 136、商城业务-首页-整合thymeleaf渲染首页

---
+++ gulimall-product/src/main/java/com/cr/gulimall/product/app/SpuInfoController.java
@@ -0,0 +1,95 @@
+package com.cr.gulimall.product.app;
+
+import com.cr.common.utils.PageUtils;
+import com.cr.common.utils.R;
+import com.cr.gulimall.product.entity.SpuInfoEntity;
+import com.cr.gulimall.product.service.SpuInfoService;
+import com.cr.gulimall.product.vo.SpuSaveVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+
+
+/**
+ * spu信息
+ *
+ * @author cr
+ * @email 931009686@qq.com
+ * @date 2021-01-25 23:32:51
+ */
+@RestController
+@RequestMapping("product/spuinfo")
+public class SpuInfoController {
+
+    @Autowired
+    private SpuInfoService spuInfoService;
+
+    /**
+     * 商品上架
+     */
+    @PostMapping("/{spuId}/up")
+    //@RequiresPermissions("product:spuinfo:list")
+    public R spuUp(@PathVariable("spuId") Long spuId) {
+        spuInfoService.up(spuId);
+
+        return R.ok();
+    }
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    //@RequiresPermissions("product:spuinfo:list")
+    public R list(@RequestParam Map<String, Object> params) {
+        PageUtils page = spuInfoService.queryPageByCondition(params);
+
+        return R.ok().put("page", page);
+    }
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    //@RequiresPermissions("product:spuinfo:info")
+    public R info(@PathVariable("id") Long id) {
+        SpuInfoEntity spuInfo = spuInfoService.getById(id);
+
+        return R.ok().put("spuInfo", spuInfo);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    //@RequiresPermissions("product:spuinfo:save")
+    public R save(@RequestBody SpuSaveVo vo) {
+        spuInfoService.saveSpuInfo(vo);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    //@RequiresPermissions("product:spuinfo:update")
+    public R update(@RequestBody SpuInfoEntity spuInfo) {
+        spuInfoService.updateById(spuInfo);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    //@RequiresPermissions("product:spuinfo:delete")
+    public R delete(@RequestBody Long[] ids) {
+        spuInfoService.removeByIds(Arrays.asList(ids));
+
+        return R.ok();
+    }
+
+}
