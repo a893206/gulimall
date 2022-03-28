7d98f182a8df23e88c9905f9143beb4a2ee8c034
CR <931009686@qq.com>
2022-03-28 22:48:33 +0800

    feat: 152、缓存-缓存使用-整合redis测试

--- gulimall-product/src/test/java/com/cr/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/cr/gulimall/product/GulimallProductApplicationTests.java
@@ -6,9 +6,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -18,6 +21,9 @@ public class GulimallProductApplicationTests {
     @Autowired
     private CategoryService categoryService;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     public void contextLoads() {
     }
@@ -26,6 +32,18 @@ public class GulimallProductApplicationTests {
     public void testFindPath() {
         Long[] catalogPath = categoryService.findCatalogPath(225L);
         log.info("完整路径：{}", Arrays.toString(catalogPath));
+    }
+
+    @Test
+    public void testStringRedisTemplate() {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+
+        // 保存
+        ops.set("hello", "value_" + UUID.randomUUID());
+
+        // 查询
+        String hello = ops.get("hello");
+        System.out.println("之前保存的数据是：" + hello);
     }
 
 }
