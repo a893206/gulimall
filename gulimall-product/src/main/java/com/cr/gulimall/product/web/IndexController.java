5ec4d61be303fe1c379a0f934551937dbaca9419
CR <931009686@qq.com>
2022-03-29 22:57:02 +0800

    feat: 160、缓存-分布式锁-Redisson-lock锁测试

--- gulimall-product/src/main/java/com/cr/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/cr/gulimall/product/web/IndexController.java
@@ -2,6 +2,8 @@ package com.cr.gulimall.product.web;
 
 import com.cr.gulimall.product.entity.CategoryEntity;
 import com.cr.gulimall.product.service.CategoryService;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +21,9 @@ import java.util.Map;
 public class IndexController {
     @Autowired
     private CategoryService categoryService;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
     @GetMapping({"/", "/index.html"})
     public String indexPage(Model model) {
@@ -38,6 +43,25 @@ public class IndexController {
     @ResponseBody
     @GetMapping("/hello")
     public String hello() {
+        // 1、获取一把锁，只要锁的名字一样，就是同一把锁
+        RLock lock = redissonClient.getLock("my-lock");
+
+        // 2、加锁
+        // 阻塞式等待。默认加的锁都是30s时间。
+        lock.lock();
+        // 1、锁的自动续期，如果业务超长，运行期间自动给锁续上新的30s。不用担心业务时间超长，锁自动过期被删掉
+        // 2、加锁的业务只要运行完成，就不会给当前锁续期，即使不手动解锁，锁默认在30s以后自动删除。
+        try {
+            System.out.println("加锁成功，执行业务……" + Thread.currentThread().getId());
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // 3、解锁 假设解锁代码没有运行，redisson会不会出现死锁
+            System.out.println("释放锁……" + Thread.currentThread().getId());
+            lock.unlock();
+        }
+
         return "hello";
     }
 }
